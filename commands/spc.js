0283bc4f1a26ade8ec999381f3e0c0b39de5bcd3
iFirefox <76655351+iifirefox@users.noreply.github.com>
2021-08-26 14:43:45 -0400

    Update spc.js

--- commands/spc.js
+++ commands/spc.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 module.exports.run = async (message, arg, User,client) => {
 if(User.id=="265733605077155851"){
     if(arg.includes("off")){
-        message.channel.send("Good Night").then(client.destory());
+        message.channel.send("Good Night").then(client.destroy());
     }
    else if(arg.includes("restart")){
        message.channel.send(":gear: *restarting...*").then((message)=>{
-        client.destory().then(client.login(process.env.tokena)).then(message.edit(":gear: restarted back for more"))});
+        client.destroy().then(client.login(process.env.tokena)).then(message.edit(":gear: restarted back for more"))});
     }
 }
 else message.channel.send(":x: Only my Developer can use this command")
