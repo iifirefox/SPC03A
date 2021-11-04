884d7281ff63281ef0bedef16d63b2dbd9efa399
iFirefox <76655351+iifirefox@users.noreply.github.com>
2021-11-04 05:15:31 -0400

    Update despawn.js

--- commands/despawn.js
+++ commands/despawn.js
@@ -14,7 +14,8 @@ module.exports.run = async (message, arg, User) => {
     User.TemdataNumbers = "";
     User.CombatMode=0;
     dembed.setAuthor(name+" has despawned",img);}
-    else if(User.CombatMode!=0||User.CombatMode==0){dembed.setDescription(":x: can only despawn spawned monsters from `spawn` command.")}
+    else if(User.CombatMode!=0){dembed.setDescription(":x: can only despawn spawned monsters from `spawn` command.")}
+    else if(User.CombatMode==0){dembed.setDescription(":x: You are not in combat.")}
     else if(!User.energy){dembed.setDescription(":x: User missing info to use this command.")};
          message.channel.send( dembed );
 }
