6090e16657a6d096bbb2d46ef88e1dd1b3436d56
iFirefox <76655351+iifirefox@users.noreply.github.com>
2021-11-04 05:04:31 -0400

    Update despawn.js

--- commands/despawn.js
+++ commands/despawn.js
@@ -3,15 +3,17 @@ module.exports.run = async (message, arg, User) => {
 
     const dembed = new Discord.MessageEmbed();
     dembed.setColor("#fdffff");
-    if(User.TemdataNames.includes("no.")){
+    var temdatanames = User.TemdataNames.split("<:>");
+    if(temdatanames[0].includes("no.")){
     var Imgset = User.Ary_Imgset.split("<:>");
     var img =Imgset[1];
+    var name = temdatanames[0];
     Imgset[1]="";
     User.Ary_Imgset = Imgset.join("<:>");
     User.TemdataNames = "";
     User.TemdataNumbers = "";
     User.CombatMode=0;
-    dembed.setAuthor(":gear: has despawned",img);}
+    dembed.setAuthor(name+" has despawned",img);}
     else if(User.CombatMode!=0){dembed.setDescription(":x: can only despawn spawned monsters from `spawn` command.")}
     else if(!User.energy){dembed.setDescription(":x: User missing info to use this command.")};
          message.channel.send( dembed );
